7855c1777268f8049df5009e01896e0f4487c169
Maxime <Maxime@MaximePc.local>
2017-10-03 09:08:53 -0400

    ok

--- MVC/model/24juin_teacher.php
+++ MVC/model/24juin_teacher.php
@@ -1,58 +1,68 @@
 <?php
-require_once 'baseModel.php';
+require_once 'BaseModel.php';
 class Prof extends BaseModel {
 	protected $table_name = 'teacher';
 	protected $primary_key = "id_teacher";
-	protected $pk_prof;
-	protected $nom;
-	protected $prenom;
+	protected $id_teacher;
+	protected $first_name;
+	protected $family_name;
 
-	public function setPk_prof($aPK) {
-		$this->pk_prof = $aPK;
-	}
-	public function getPk_prof() {
-		return $this->pk_prof;
-	}
-	
-	/**
-	 * nom
-	 * 
-	 * @return unkown
-	 */
-	public function getNom() {
-		return $this->nom;
-	}
-	
-	/**
-	 * nom
-	 * 
-	 * @param unkown $nom        	
-	 * @return Client
-	 */
-	public function setNom($nom) {
-		$this->nom = $nom;
-		return $this;
-	}
-	
-	/**
-	 * prenom
-	 * 
-	 * @return unkown
-	 */
-	public function getPrenom() {
-		return $this->prenom;
-	}
-	
-	/**
-	 * prenom
-	 * 
-	 * @param unkown $prenom        	
-	 * @return Client
-	 */
-	public function setPrenom($prenom) {
-		$this->prenom = $prenom;
-		return $this;
-	}
 	
 	
+	
+
+    /**
+     * id_teacher
+     * @return int
+     */
+    public function getId_teacher(){
+        return $this->id_teacher;
+    }
+
+    /**
+     * id_teacher
+     * @param int $id_teacher
+     * @return Prof
+     */
+    public function setId_teacher($id_teacher){
+        $this->id_teacher = $id_teacher;
+        return $this;
+    }
+
+    /**
+     * first_name
+     * @return string
+     */
+    public function getFirst_name(){
+        return $this->first_name;
+    }
+
+    /**
+     * first_name
+     * @param string $first_name
+     * @return Prof
+     */
+    public function setFirst_name($first_name){
+        $this->first_name = $first_name;
+        return $this;
+    }
+
+    /**
+     * family_name
+     * @return string
+     */
+    public function getFamily_name(){
+        return $this->family_name;
+    }
+
+    /**
+     * family_name
+     * @param string $family_name
+     * @return Prof
+     */
+    public function setFamily_name($family_name){
+        $this->family_name = $family_name;
+        return $this;
+    }
+
 }
