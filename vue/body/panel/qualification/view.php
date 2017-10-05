680631ddaef67a014caf7ee290351177a6d79e8c
primopietro <Pietro.hyde@gmail.com>
2017-10-04 20:40:57 -0400

    ok

--- vue/body/panel/qualification/view.php
+++ vue/body/panel/qualification/view.php
@@ -2,24 +2,28 @@
 if (! isset ( $_SESSION )) {
 	session_start ();
 }
-require_once $_SERVER["DOCUMENT_ROOT"] . '/24juin/MVC/model/24juin_qualification.php';
-require_once $_SERVER["DOCUMENT_ROOT"] . '/24juin/vue/rightHelper.php';
+require_once $_SERVER ["DOCUMENT_ROOT"] . '/24juin/MVC/model/24juin_qualification.php';
+require_once $_SERVER ["DOCUMENT_ROOT"] . '/24juin/vue/rightHelper.php';
 
 $objName = "qualification";
-$rights =checkUserRights($objName, $_SESSION ['rightList']);
-//Check if user has the right to VIEW
+$rights = checkUserRights ( $objName, $_SESSION ['rightList'] );
+// Check if user has the right to VIEW
 
-if(isset($rights['view'])){
+if (isset ( $rights ['view'] )) {
+	
+	$aQualification = new Qualification ();
+	$aListOfQualifications = $aQualification->getActiveQualifications ();
+	
 	$default = "
    <section class='content'>
     <div class='row'>
         <div class='col-xs-12'>
           <div class='box'>
             <div class='box-header'>
-              <h3 class='box-title'>Liste ".$objName."</h3>";
-			
-	if(isset($rights['add'])){
-		$default .= "<br>  <a class='action' action='add' objtype='".$objName."'>Ajouter </a>";
+              <h3 class='box-title'>Liste " . $objName . "</h3>";
+	
+	if (isset ( $rights ['add'] )) {
+		$default .= "<br>  <a class='action' action='add' objtype='" . $objName . "'>Ajouter </a>";
 	}
 	$default .= "<div class='box-tools'>
 			
@@ -30,31 +34,34 @@ if(isset($rights['view'])){
 			
                             </div>
             <!-- /.box-header -->
-            <div class='box-body'>
-              <table class='table table-bordered table-hover'>
-             <thead>
-                <tr>
-                  <th>Code </th>
-                  <th>Nom</th>
-				  <th>Quantite d'heures</th>";
-	if(isset($rights['update'])){
-		$default .= "  <th>Modifier</th>";
-	}
-	if(isset($rights['delete'])){
-		$default .= "  <th>Supprimer</th>";
-	}
-	
-	$default .= "</tr>
+            <div class='box-body'>";
+	if ($aListOfQualifications != null) {
+		if (sizeof ( $aListOfQualifications ) > 0) {
+			$default .= "<table class='table table-bordered table-hover'>
+			<thead>
+			<tr>
+			<th>Code </th>
+			<th>Nom</th>
+			<th>Quantite d'heures</th>";
+			if (isset ( $rights ['update'] )) {
+				$default .= "  <th>Modifier</th>";
+			}
+			if (isset ( $rights ['delete'] )) {
+				$default .= "  <th>Supprimer</th>";
+			}
+			
+			$default .= "</tr>
             <thead>
             <tbody>";
+			
+			$default .= $aQualification->printQualificationsList ( $aListOfQualifications, isset ( $rights ['update'] ), isset ( $rights ['delete'] ) );
+			
+			$default .= "</tbody>
+              </table>";
+		}
+	}
 	
-	$aQualification = new Qualification();
-	$aListOfQualifications = $aQualification->getActiveQualifications();
-	$default .= $aQualification->printQualificationsList($aListOfQualifications,isset($rights['update']),isset($rights['delete']));
-	
-	$default .= "</tbody>
-              </table>
-            </div>
+	$default .= "</div>
             <!-- /.box-body -->
           </div>
           <!-- /.box -->
@@ -65,7 +72,6 @@ if(isset($rights['view'])){
     <!-- /.content -->";
 	
 	echo $default;
-	
 }
 
    
