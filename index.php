8a8bdf961c4403a1ceea14f6005f195eb65bd1f3
ZoreticBoris <ZoreticBoris@DESKTOP-VJ2LHJH>
2017-10-25 21:29:41 -0400

    réglage style

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
 if(!isset($_SESSION)){session_start();}
 require_once "system/header.php";
 if(isset($_GET['actions']) ||isset( $_SESSION['current_page'])){
-    require_once "vue/header/header.php";
+	require_once "vue/header/header.php";
+	echo "<div class='content-wrapper' id='mainContent' style='min-height: 100vh;'>";
+} else{
+	echo "<div class='content-wrapper' id='mainContent' style='min-height: 100vh; margin: auto; width: 1000px;'>";
 }
 
 ?>
 
-<div class="content-wrapper" id='mainContent' style="min-height: 100vh; ">
+
+
 <?php 
 
 require_once "vue/body/viewManager.php";
