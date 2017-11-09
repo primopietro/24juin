e341c2c2b8a980cd605e6c27620d0ef8f55f75ba
primopietro <Pietro.hyde@gmail.com>
2017-11-08 19:54:10 -0500

    ok

--- testPage.php
+++ testPage.php
@@ -1,25 +1,359 @@
 
 <?php 
-//FOr admin
-$query= "INSERT INTO `right_object_role` (`id_right_object_role`, `id_right`, `id_object`, `id_role`) 
-VALUES ";
-//for each object
-for($i=1;$i<=10;$i++){
-	//for each right
-
-	
-		$query .="(NULL,'";
-		$query .=3;
-		$query .="','";
-		$query .=$i;
-		$query .="','";
-		$query .="2'";
-		$query .=")";
-		
-		
-	if($i < 10){
-		$query .=",";
-	}
+if(!isset($_SESSION)){session_start();}
+require_once "system/header.php";
+if(isset($_GET['actions']) ||isset( $_SESSION['current_page'])){
+	require_once "vue/header/header.php";
+	echo "<div class='content-wrapper' id='mainContent' style='min-height: 100vh;'>";
+} else{
+	echo "<div class='content-wrapper' id='mainContent' style='min-height: 100vh; margin: auto; width: 1000px;'>";
 }
-echo $query;
+
 ?>
+
+<?php 
+
+//require_once "vue/body/viewManager.php";
+
+$dataForSchedule ='[{
+	"year": "2017-2018",
+	"group": "1",
+	"program": "1",
+	"am": {
+		"am1": {
+			"time_start": "08:15",
+			"time_end": "10:15"
+
+		},
+		"am2": {
+			"time_start": "10:30",
+			"time_end": "11:30"
+		}
+	},
+	"pm": {
+		"pm1": {
+			"time_start": "12:30",
+			"time_end": "14:30"
+
+		},
+		"pm2": {
+			"time_start": "14:45",
+			"time_end": "15:45"
+		}
+	},
+	"weeks": [{
+		"id_week": "1",
+		"monday": {
+			"am1": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			},
+			"am2": null,
+			"pm1": null,
+			"pm2": null
+		},
+		"tuesday": null,
+		"wednesday": null,
+		"thursday": null,
+		"friday": null
+	}, {
+		"id_week": "2",
+		"monday": {
+			"am1": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			},
+			"am2": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			},
+			"pm1": null,
+			"pm2": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			}
+		},
+		"tuesday": null,
+		"wednesday": null,
+		"thursday": null,
+		"friday": {
+			"am1": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			},
+			"am2": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			},
+			"pm1": null,
+			"pm2": {
+				"timeslot": {
+					"id_timeslot": "1",
+					"teacher": [{
+							"id_teacher": "1"
+						},
+						{
+							"id_teacher": "2"
+						}
+					],
+					"qualification_teached": [
+
+						{
+							"id_qualification_teached": "1"
+						},
+						{
+							"id_qualification_teached": "2"
+						}
+
+					],
+					"id_week": 1,
+					"classroom": [{
+						"id_classroom": "1",
+						"zone": [{
+								"id_zone": "1"
+							},
+							{
+								"id_zone": "2"
+							}
+						]
+					}],
+					"isExam": "false",
+					"isPedagoDayProgram": "false",
+					"isPegadoDayAll": "false",
+					"isFixedHoliday": "false",
+					"isStageIndividual": "false",
+					"isStageAccompanied": "false",
+					"isSpecialEvent": "false"
+				}
+			}
+		}
+	}]
+}]';
+$dataForSchedule = json_decode($dataForSchedule, true);
+print_r ($dataForSchedule);
+
+
+
+?>
+
+</div>
+<?php 
+require_once "system/footer.php";
+?>
+
---
+++ vue/body/panel/schedule/view.php
@@ -0,0 +1,77 @@
+<?php
+if (! isset ( $_SESSION )) {
+    session_start ();
+}
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/24juin/MVC/model/24juin_program.php';
+require_once $_SERVER ["DOCUMENT_ROOT"] . '/24juin/vue/rightHelper.php';
+
+$objName = "program";
+$rights = checkUserRights ( $objName, $_SESSION ['rightList'] );
+
+if (isset ( $rights ['view'] )) {
+    
+    $aProgram = new Program();
+    $aListOfProgram = $aProgram->getActiveProgram();
+    
+    $default = "
+   <section class='content'>
+    <div class='row'>
+        <div class='col-xs-12'>
+          <div class='box'>
+            <div class='box-header'>
+              <h3 class='box-title'>Liste des programmes</h3>";
+    
+    if (isset ( $rights ['add'] )) {
+        $default .="<br><a class='btn btn-app action'  action='add' objtype='" . $objName . "'>
+                <i class='fa fa-plus'></i> Ajouter
+              <div class='ripple-container'></div></a>";}
+
+
+    $default .= "<div class='box-tools'>
+                                
+                               <button type='button' class='btn btn-box-tool'
+								data-widget='collapse'>
+								<i class='fa fa-minus'></i>
+							</button>
+                               
+                            </div>
+            <!-- /.box-header -->
+            <div class='box-body'>";
+    if ($aListOfProgram != null) {
+        if (sizeof ( $aListOfProgram ) > 0) {
+		    $default .= "<table class='table table-bordered table-hover'>
+             <thead>
+                <tr>
+                  <th>Nom</th>
+                  <th>Durée</th>
+                  <th>Nb. compétences</th>";
+                if (isset ( $rights ['update'] )) {
+				    $default .= "<th>Modifier</th>";
+			    }
+			    if (isset ( $rights ['delete'] )) {
+				    $default .= "<th>Supprimer</th>";
+			    }
+			    $default .= "</tr>
+            <thead>
+            <tbody>";
+			    $default .= $aProgram->printProgramList($aListOfProgram, isset ( $rights ['update'] ), isset ( $rights ['delete'] ));
+            
+			$default .= "</tbody>
+              </table>";
+        }
+    }
+    
+    
+    $default .= "</div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+
+    </section>
+    <!-- /.content -->";
+    
+    echo $default;
+}
