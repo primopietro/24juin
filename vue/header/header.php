3154e4da3fe5a8617906a39f765e49a3ad9f8951
TheBeast4QC <alexrioux81@hotmail.com>
2017-12-02 19:01:38 -0800

    ok

--- vue/header/header.php
+++ vue/header/header.php
@@ -258,23 +258,7 @@ function getSubMenuItem($localItem,$tempRights){
             
         }
     }
-    if($localItem ['object'] ['name'] == "teacher"){
-        foreach ($tempRights as $localItem ) {
-            
-            if($localItem ['object'] ['name'] == "teacher_qualification_teached"){
-                foreach ( $localItem ['rights'] as $aLocalRight ) {
-                    $right=frenchTranslator ($aLocalRight['name']);
-                    if($aLocalRight['name'] == "view"  ){
-                        
-                        $default .= " <li navigation='" . $localItem ['object'] ['name'] . "'><a class='action' action='" . $aLocalRight['name'] . "' ><i class='fa fa-circle-o'></i>Assigner des compétence enseigné</a></li> ";
-                        
-                        
-                    }
-                }
-            }
-            
-        }
-    }
+
     return $default;
 }
 
